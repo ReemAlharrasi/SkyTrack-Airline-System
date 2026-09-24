42478e46792df50a41bae56a2eb3aecb087b0cf9
Reem Al Harrasi <Reemalharrasi00@gmail.com>
2026-09-24 10:05:29 +0400

    done advanced queries and update readme file

--- 08-Queries-Advanced.sql
+++ 08-Queries-Advanced.sql
@@ -1,17 +1,54 @@
 USE SkyTrack;
 
 -- ============== Advanced Level ==============
--- 1. Show each flight with its flight number, origin airport, destination airport, aircraft model, and the total number of passengers booked on it. Include flights that have no bookings.
+-- 1. Show each flight with its flight number, origin airport, destination airport, aircraft model, 
+-- and the total number of passengers booked on it.  Include flights that have no bookings. -> left join
+SELECT f.Flight_number, o.Name, d.Name, a.Model, COUNT(b.Booking_id) AS total_passengers
+FROM Flight f
+JOIN Airport o on f.Departing_Airport_code = o.Airport_id
+JOIN Airport d on f.Arriving_Airport_code = d.Airport_id
+JOIN Aircraft a on f.Aircraft_Registeration_Number = a.Aircraft_id
+LEFT JOIN Booking b ON f.Flight_id = b.Flight_id
+GROUP BY  f.Flight_number, o.Name, d.Name, a.Model;
 
 -- 2. List all passengers who have never made a booking.
+SELECT *
+FROM Passenger p
+WHERE p.Passenger_id NOT IN (SELECT Passenger_id FROM Booking);
 
--- 3. For each flight, show the flight number and the total revenue generated from its bookings. Show only flights where the total revenue exceeds 500. Order from highest to lowest.
+-- 3. For each flight, show the flight number and the total revenue generated from its bookings. 
+-- Show only flights where the total revenue exceeds 500. Order from highest to lowest.
+SELECT f.Flight_number, SUM(b.price_paid) AS Total_revenue
+FROM Flight f
+JOIN Booking b ON f.Flight_id = b.Flight_id
+GROUP BY f.Flight_number
+HAVING SUM(b.price_paid)>50
+ORDER BY Total_revenue DESC;
 
--- 4. Show each crew member's full name and the total number of flights they have been assigned to. Show only crew members assigned to more than one flight.
+-- 4. Show each crew member's full name and the total number of flights they have been assigned to. 
+-- Show only crew members assigned to more than one flight.
+SELECT c.FullName, SUM(cf.flight_id) as total_flights
+FROM Crew_Member c
+JOIN Flight_crew cf ON c.Crew_id = cf.Crew_id
+GROUP BY c.FullName
+HAVING SUM(cf.flight_id) > 1;
 
 -- 5. Find the average booking price per flight. Show only flights where the average price is above the overall average price across all bookings.
+SELECT f.Flight_number,AVG(b.price_paid) AS average_price --this gives avg for price paid for each flight alone
+FROM Flight f
+JOIN Booking b ON f.Flight_id = b.Flight_id
+GROUP BY f.Flight_number
+HAVING AVG(b.price_paid) > (SELECT AVG(price_paid) FROM Booking)  ; -- this gives the average across all flights
 
 -- 6. Show the flight with the highest number of bookings. Display its flight number, origin, destination, and total bookings.
+SELECT TOP 1 
+f.Flight_number, o.Name, d.Name, COUNT(b.Booking_id) AS Total_Bookings
+FROM Flight f
+JOIN Airport o on f.Departing_Airport_code = o.Airport_id
+JOIN Airport d on f.Arriving_Airport_code = d.Airport_id
+JOIN Booking b ON b.Flight_id = f.Flight_id
+GROUP BY f.Flight_number, o.Name, d.Name
+ORDER BY Total_Bookings DESC;
 
 -- 7. For each booking class, show the total revenue, the number of bookings, the average price, the highest price, and the lowest price.
 
--- readme.txt
+++ readme.txt
@@ -1,5 +1,5 @@
 ERD ===================================
-ERD is straight forward and easy to draw
+ERD is straight forward and easy to draw.
 
 MAPPING ===============================
 mapping was easy as FK are simple and forward and easy to understand. But I need to go back and add the primary keys that i decided on later.
@@ -17,3 +17,99 @@ Completion time: 2026-09-23T15:48:27.8743846+04:00
 SOLution: wrong order of columns and values
 
 
+
+DQL ===================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+SELECT b.Booking_id, SUM(b.price_paid) AS total_revenue, COUNT(b.Booking_id), AVG(b.price_paid), MAX(b.price_paid), MIN(b.price_paid)
+FROM Booking b;
+
+error:
+Msg 8120, Level 16, State 1, Line 55
+Column 'Booking.Booking_id' is invalid in the select list because it is not contained in either an aggregate function or the GROUP BY clause.
+
+
+fix? add GROUP by
+
+--------------
+SELECT b.Booking_id, SUM(b.price_paid) AS total_revenue, COUNT(b.Booking_id), AVG(b.price_paid), MAX(b.price_paid), MIN(b.price_paid)
+FROM Booking b
+GROUP BY b.Booking_id;
+
+issue: column headers are empty for some
+
+Fix? use AS <alias>
+-------------------
+
+Msg 8120, Level 16, State 1, Line 37
+Column 'Booking.price_paid' is invalid in the select list because it is not contained in either an aggregate function or the GROUP BY clause.
+
+Completion time: 2026-09-23T17:44:29.4369239+04:00
+
+
+Solution: forgot to write the aggregate function name
+
+------------
+Msg 8120, Level 16, State 1, Line 6
+Column 'Flight.Flight_number' is invalid in the select list because it is not contained in either an aggregate function or the GROUP BY clause.
+
+
+Solution: use group by because we are using aggregate functions
+--------------
+
+SELECT f.Flight_number, SUM(b.price_paid) AS Total_revenue
+FROM Flight f
+JOIN Booking b ON f.Flight_id = b.Flight_id
+GROUP BY f.Flight_number
+HAVING SUM(b.price_paid)>500
+ORDER BY Total_revenue DESC;
+
+
+this showed me an empty table, so i changed HAVING SUM(b.price_paid)>500 to this HAVING SUM(b.price_paid)>50
+
+----------------
+SELECT c.FullName, SUM(cf.flight_id) as total_flights
+FROM Crew_Member c
+JOIN Flight_crew cf ON c.Crew_id = cf.Crew_id
+GROUP BY c.FullName
+HAVING total_flights > 1;
+
+
+Msg 207, Level 16, State 1, Line 34
+Invalid column name 'total_flights'.
+
+Completion time: 2026-09-24T09:07:47.4971571+04:00
+
+
+fix? replace total_flights in last line with SUM(cf.flight_id)
+
+-----------------
+
+SELECT f.Flight_number,AVG(b.price_paid) AS average_price
+FROM Flight f
+JOIN Booking b ON f.Flight_id = b.Flight_id
+GROUP BY f.Flight_number
+HAVING price_paid> AVG(b.price_paid);
+
+Msg 8121, Level 16, State 1, Line 41
+Column 'Booking.price_paid' is invalid in the HAVING clause because it is not contained in either an aggregate function or the GROUP BY clause.
+
+Completion time: 2026-09-24T09:12:31.7375403+04:00
+
+
+fix? Having condition is wrongly worded
+replace with HAVING AVG(b.price_paid) > (SELECT AVG(price_paid) FROM Booking) 
